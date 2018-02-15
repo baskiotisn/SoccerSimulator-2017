021dbe619af9e00bbd1d15888d839cce43d53db9
Maxime Sangnier <maxime.sangnier@upmc.fr>
2018-02-15 18:25:37 +0100

    grid search

--- Exemple_GIT_REPO/profAI/__init__.py
+++ Exemple_GIT_REPO/profAI/__init__.py
@@ -1,4 +1,5 @@
-from .strategies import DefenseurStrategy,FonceurStrategy,RandomStrategy
+from .strategies import DefenseurStrategy,FonceurStrategy,RandomStrategy, FonceurTestStrategy
+from .optimization import ParamSearch
 from soccersimulator import SoccerTeam
 
 def get_team(nb_players):
