0a8f60c90673e0c93f739cf7d732391a158a30ff
Maxime Sangnier <maxime.sangnier@upmc.fr>
2018-02-20 10:16:24 +0100

    fix grid search
    Former-commit-id: a9a0a86cbd68bc54d06eb75479aef51f8beb5aa8

--- Exemple_GIT_REPO/profAI/optimization.py
+++ Exemple_GIT_REPO/profAI/optimization.py
@@ -32,9 +32,11 @@ class ParamSearch(object):
         self.last = 0  # Step of the last round
         self.crit = 0  # Criterion to maximize (here, number of goals)
         self.cpt = 0  # Counter for trials
+
+        if len(self.params) > 2:
+            raise ValueError('Max two parameters')
         self.param_keys = list(self.params.keys())  # Name of all parameters
-        self.param_id = [0] * len(self.params)  # Index of the parameter values
-        self.param_id_id = 0  # Index of the current parameter
+        self.param_id = [0] * len(self.param_keys)  # Index of the parameter values
         self.res = dict()  # Dictionary of results
 
     def begin_round(self, team1, team2, state):
@@ -66,30 +68,36 @@ class ParamSearch(object):
             self.crit += 1  # Increment criterion
 
         self.cpt += 1  # Increment number of trials
+
+        for i, (key, values) in zip(self.param_id, self.params.items()):
+            print("{}: {}".format(key, values[i]), end="   ")
+        print("Crit: {}   Cpt: {}".format(self.crit, self.cpt))
+
         if self.cpt >= self.trials:
             # Save the result
             res_key = tuple()
             for i, values in zip(self.param_id, self.params.values()):
                 res_key += values[i],
             self.res[res_key] = self.crit * 1. / self.trials
-            print(res_key, self.crit)
 
             # Reset parameters
             self.crit = 0
             self.cpt = 0
 
             # Go to the next parameter value to try
-            key = self.param_keys[self.param_id_id]
-            if self.param_id[self.param_id_id] < len(self.params[key]) - 1:
-                self.param_id[self.param_id_id] += 1
-            elif self.param_id_id < len(self.params) - 1:
-                self.param_id_id += 1
+            key0 = self.param_keys[0]
+            if self.param_id[0] < len(self.params[key0]) - 1:
+                self.param_id[0] += 1
+            elif len(self.params) > 1:
+                key1 = self.param_keys[1]
+                if self.param_id[1] < len(self.params[key1]) - 1:
+                    self.param_id[0] = 0
+                    self.param_id[1] += 1
+                else:
+                    self.simu.end_match()
             else:
                 self.simu.end_match()
 
-        for i, (key, values) in zip(self.param_id, self.params.items()):
-            print("{}: {}".format(key, values[i]), end="   ")
-        print("Crit: {}   Cpt: {}".format(self.crit, self.cpt))
 
     def get_res(self):
         return self.res
